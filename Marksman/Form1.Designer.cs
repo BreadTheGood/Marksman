c1f55b3fe220e1d0bbcc93d62e3cad51b5873836
Samuel Martínez <samuelmnqn@gmail.com>
2024-08-11 00:03:15 -0300

    Agregar archivos de proyecto.

---
+++ Marksman/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Marksman
+{
+    partial class window
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(window));
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Digital-7 Mono", 20F, FontStyle.Bold);
+            label1.Location = new Point(12, 23);
+            label1.Name = "label1";
+            label1.Size = new Size(245, 40);
+            label1.TabIndex = 0;
+            label1.Text = "Puntos = 100";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Digital-7 Mono", 20F, FontStyle.Bold);
+            label2.Location = new Point(12, 93);
+            label2.Name = "label2";
+            label2.Size = new Size(303, 40);
+            label2.TabIndex = 1;
+            label2.Text = "Accuracy = 100%";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Digital-7 Mono", 20F, FontStyle.Bold);
+            label3.Location = new Point(1659, 9);
+            label3.Name = "label3";
+            label3.Size = new Size(188, 40);
+            label3.TabIndex = 2;
+            label3.Text = "tries = 0";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Digital-7 Mono", 20F, FontStyle.Bold);
+            label4.Location = new Point(1640, 93);
+            label4.Name = "label4";
+            label4.Size = new Size(206, 40);
+            label4.TabIndex = 3;
+            label4.Text = "Missed = 0";
+            label4.Click += label4_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.BackgroundImage = (Image)resources.GetObject("pictureBox1.BackgroundImage");
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox1.Location = new Point(727, 410);
+            pictureBox1.MaximumSize = new Size(100, 100);
+            pictureBox1.MinimumSize = new Size(15, 15);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(100, 100);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            pictureBox1.LocationChanged += pictureBox1_LocationChanged;
+            pictureBox1.SizeChanged += pictureBox1_SizeChanged;
+            pictureBox1.VisibleChanged += pictureBox1_VisibleChanged;
+            pictureBox1.Click += pictureBox1_Click;
+            pictureBox1.MouseDown += pictureBox1_MouseDown;
+            pictureBox1.MouseUp += pictureBox1_MouseUp;
+            // 
+            // window
+            // 
+            AutoScaleDimensions = new SizeF(9F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            BackColor = SystemColors.ActiveCaption;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(1878, 944);
+            Controls.Add(pictureBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            DoubleBuffered = true;
+            Font = new Font("Digital-7 Mono", 8.999999F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "window";
+            Text = "Marksman";
+            Click += window_Click;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ Marksman/Form1.cs
@@ -0,0 +1,124 @@
+using System.Media;
+
+namespace Marksman
+{
+    public partial class window : Form
+    {
+        // variables
+        int puntos = 100;
+
+        double tries = 0;
+
+        double missed = 0;
+
+        double acc = 0;
+
+        double oks = 0;
+        public window()
+        {
+            InitializeComponent();
+
+            // initial rounded form
+            System.Drawing.Drawing2D.GraphicsPath gp = new System.Drawing.Drawing2D.GraphicsPath();
+            gp.AddEllipse(0, 0, pictureBox1.Width - 1, pictureBox1.Height - 1);
+            Region rg = new Region(gp);
+            pictureBox1.Region = rg;
+
+        }
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            //sound
+
+            SoundPlayer shot = new SoundPlayer(@"C:\Users\NSX\source\repos\Marksman\Marksman\bin\Assets\shot.wav");
+            shot.Play();
+
+
+            pictureBox1.Visible = false;
+            //object new random location and size
+            Random rnd = new Random();
+            int num1 = rnd.Next(100, 1500);
+            int num2 = rnd.Next(100, 800);
+            int rndSize = rnd.Next(30, 100);
+            //object relocation and resizing
+            pictureBox1.Location = new System.Drawing.Point(num1, num2);
+            pictureBox1.Width = rndSize;
+            pictureBox1.Height = rndSize;
+
+            //score calculation
+            puntos--;
+            tries++;
+            oks++;
+
+            acc = Math.Round((oks * 100) / tries, 2);
+
+            //display score
+            label2.Text = " Accuracy =" + acc + "%";
+
+            label3.Text = "tries =" + tries;
+
+            label1.Text = "Puntos = " + puntos.ToString();
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void pictureBox1_VisibleChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_SizeChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_LocationChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            //relocating delay
+            System.Threading.Thread.Sleep(500);
+
+            //
+            pictureBox1.Visible = true;
+            // initial rounded form
+            System.Drawing.Drawing2D.GraphicsPath gp = new System.Drawing.Drawing2D.GraphicsPath();
+            gp.AddEllipse(0, 0, pictureBox1.Width - 1, pictureBox1.Height - 1);
+            Region rg = new Region(gp);
+            pictureBox1.Region = rg;
+
+            //sound
+
+            SoundPlayer appear = new SoundPlayer(@"C:\Users\NSX\source\repos\Marksman\Marksman\bin\Assets\target.wav");
+            appear.Play();
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void window_Click(object sender, EventArgs e)
+        {
+            missed++;
+            tries++;
+
+            acc = Math.Round((oks * 100) / tries, 2);
+
+            label2.Text = " Accuracy =" + acc + "%";
+
+            label3.Text = "tries =" + tries;
+
+            label4.Text = "Missed = " + missed;
+
+            label1.Text = "Puntos = " + puntos.ToString();
+        }
+    }
+}
