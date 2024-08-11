66d8102c2956b7bad9b915c5904e6b8bccc0f70a
Samuel Martínez <samuelmnqn@gmail.com>
2024-08-11 00:46:52 -0300

    target images

--- Marksman/Form1.cs
+++ Marksman/Form1.cs
@@ -6,6 +6,8 @@ namespace Marksman
 {
     public partial class window : Form
     {
+        
+
         // variables
         int puntos = 100;
 
@@ -18,11 +20,9 @@ namespace Marksman
         double oks = 0;
         public window()
         {
-            InitializeComponent();
+            InitializeComponent();            
 
-            
-
-            //Cursor.Current = new Cursor(@"C:\Users\NSX\source\repos\Marksman\Marksman\bin\Assets\cursor.cur");
+            Cursor.Current = new Cursor(@"C:\Users\NSX\source\repos\Marksman\Marksman\bin\Assets\cursor.cur");
 
             // initial rounded form
             System.Drawing.Drawing2D.GraphicsPath gp = new System.Drawing.Drawing2D.GraphicsPath();
@@ -33,18 +33,20 @@ namespace Marksman
         }
         private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
         {
-            //sound
+            //shot sound
 
             SoundPlayer shot = new SoundPlayer(@"C:\Users\NSX\source\repos\Marksman\Marksman\bin\Assets\shot.wav");
             shot.Play();
 
-
+            //hide target
             pictureBox1.Visible = false;
+
             //object new random location and size
             Random rnd = new Random();
             int num1 = rnd.Next(100, 1500);
             int num2 = rnd.Next(100, 800);
             int rndSize = rnd.Next(30, 100);
+
             //object relocation and resizing
             pictureBox1.Location = new System.Drawing.Point(num1, num2);
             pictureBox1.Width = rndSize;
@@ -54,7 +56,6 @@ namespace Marksman
             puntos--;
             tries++;
             oks++;
-
             acc = Math.Round((oks * 100) / tries, 2);
 
             //display score
@@ -87,19 +88,27 @@ namespace Marksman
 
         private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
         {
+            //ramdon img selection
+            Random rnd = new Random();
+            int num3 = rnd.Next(1, 3);
+            if (num3==1) {
+                pictureBox1.BackgroundImage = Image.FromFile(@"C:\Users\NSX\source\repos\Marksman\Marksman\bin\Assets\sphere3.png");
+            } else {
+                pictureBox1.BackgroundImage = Image.FromFile(@"C:\Users\NSX\source\repos\Marksman\Marksman\bin\Assets\sphere 2.png");
+            }
             //relocating delay
             System.Threading.Thread.Sleep(500);
 
-            //
+            //show new target
             pictureBox1.Visible = true;
+
             // initial rounded form
             System.Drawing.Drawing2D.GraphicsPath gp = new System.Drawing.Drawing2D.GraphicsPath();
             gp.AddEllipse(0, 0, pictureBox1.Width - 1, pictureBox1.Height - 1);
             Region rg = new Region(gp);
             pictureBox1.Region = rg;
 
-            //sound
-
+            //appear sound
             SoundPlayer appear = new SoundPlayer(@"C:\Users\NSX\source\repos\Marksman\Marksman\bin\Assets\target.wav");
             appear.Play();
 
@@ -126,8 +135,7 @@ namespace Marksman
 
             label1.Text = "Puntos = " + puntos.ToString();
 
-            //sound
-
+            //shot sound
             SoundPlayer shot = new SoundPlayer(@"C:\Users\NSX\source\repos\Marksman\Marksman\bin\Assets\shot.wav");
             shot.Play();
         }
